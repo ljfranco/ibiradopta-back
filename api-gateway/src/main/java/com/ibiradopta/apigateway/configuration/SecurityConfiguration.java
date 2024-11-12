ed8f65f7c4ea0354affa8ec3543fb5158fbc1493
Leonardo Franco <leonardojfs83@gmail.com>
2024-11-12 18:07:39 -0300

    Implementation of environment variables to all services

--- api-gateway/src/main/java/com/ibiradopta/apigateway/configuration/SecurityConfiguration.java
+++ api-gateway/src/main/java/com/ibiradopta/apigateway/configuration/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.ibiradopta.apigateway.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
--- project-service/src/main/java/com/ibiradopta/project_service/feignClient/UserClient.java
+++ project-service/src/main/java/com/ibiradopta/project_service/feignClient/UserClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "user-service", url = "http://localhost:8081", configuration = OAuthFeignConfig.class)
+@FeignClient(name = "user-service", url = "${USER_SERVICE_URL}", configuration = OAuthFeignConfig.class)
 public interface UserClient {
     @RequestMapping(method = RequestMethod.GET, value = "/users/id/{id}")
     UserDto getUserById(@PathVariable("id") String id);

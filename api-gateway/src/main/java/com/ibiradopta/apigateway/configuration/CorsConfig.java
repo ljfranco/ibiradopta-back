22b7483a7b1607bc0098720735b45643681520e6
Leonardo Franco <leonardojfs83@gmail.com>
2024-11-14 10:30:00 -0300

    Dockerfile creation

---
+++ api-gateway/src/main/java/com/ibiradopta/apigateway/configuration/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.ibiradopta.apigateway.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return  source;
+    }
+}

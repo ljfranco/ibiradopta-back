8bbd2ffd5e0c70509d8f4b96268bf9b70df100ae
Leonardo Franco <leonardojfs83@gmail.com>
2024-11-24 21:23:07 -0300

    routes fixes

--- project-service/src/main/java/com/ibiradopta/project_service/controller/PaymentController.java
+++ project-service/src/main/java/com/ibiradopta/project_service/controller/PaymentController.java
@@ -2,15 +2,17 @@ package com.ibiradopta.project_service.controller;
 
 import com.ibiradopta.project_service.models.Payment;
 import com.ibiradopta.project_service.models.dto.PaymentDto;
+import com.ibiradopta.project_service.models.dto.ProjectDto;
+import com.ibiradopta.project_service.services.impl.MercadoPagoService;
 import com.ibiradopta.project_service.services.impl.PaymentService;
+import com.mercadopago.exceptions.MPApiException;
+import com.mercadopago.exceptions.MPException;
+import com.mercadopago.resources.preference.Preference;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +37,15 @@ public class PaymentController {
             @RequestParam(required = false) String startDate,
             @RequestParam(required = false) String endDate) {
         return ResponseEntity.ok(paymentService.getPaymentsByFilters(projectId, userId, startDate, endDate));
+    }
+    @Autowired
+    private MercadoPagoService mercadoPagoService;
+
+    // Endpoint para crear preferencia
+    @PostMapping("/createpreference")
+    public Preference newPreference(@RequestBody List<ProjectDto> projects) throws MPException, MPApiException {
+
+        // Crear la preferencia con los productos recibidos
+        return mercadoPagoService.createPreference(projects);
     }
 }

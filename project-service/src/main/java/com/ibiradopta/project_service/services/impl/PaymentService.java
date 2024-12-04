69b0ff890194017e694fdf6ba17e86aa134587dd
Leonardo Franco <leonardojfs83@gmail.com>
2024-12-04 09:47:28 -0300

    debug log save to BBDD

--- project-service/src/main/java/com/ibiradopta/project_service/services/impl/PaymentService.java
+++ project-service/src/main/java/com/ibiradopta/project_service/services/impl/PaymentService.java
@@ -76,6 +76,8 @@ public class PaymentService implements IPaymentService {
                     return payment;
                 })
                 .collect(Collectors.toList());
+
+        System.out.println("payments to bbdd"+payments.get(0).toString())
         paymentRepository.saveAll(payments);
 
         // Enviar un correo electrónico al usuario con la confirmación del pago

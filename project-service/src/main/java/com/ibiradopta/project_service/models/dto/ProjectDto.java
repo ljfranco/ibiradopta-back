49aa5af6b2eba9f3e91b670b64e391685d87dcc6
Leonardo Franco <leonardojfs83@gmail.com>
2024-12-11 01:18:07 -0300

    minor fix

--- project-service/src/main/java/com/ibiradopta/project_service/models/dto/ProjectDto.java
+++ project-service/src/main/java/com/ibiradopta/project_service/models/dto/ProjectDto.java
@@ -1,10 +1,12 @@
 package com.ibiradopta.project_service.models.dto;
 
+import com.ibiradopta.project_service.models.Image;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,6 +20,7 @@ public class ProjectDto {
     private String location;
     private Integer quantity;
     private BigDecimal price;
+    private List<Image> images;
 
 
 
